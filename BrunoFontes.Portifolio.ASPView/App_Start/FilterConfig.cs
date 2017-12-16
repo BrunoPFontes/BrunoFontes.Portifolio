837d9603698b5c5c729e7c49a22a3c9e622e7074
BrunoPFontes <brunopedrosafontes@gmail.com>
2017-12-15 19:51:25 -0800

    2017-12-15

---
+++ BrunoFontes.Portifolio.ASPView/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BrunoFontes.Portifolio.ASPView
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BrunoFontes.Portifolio.ASPView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BrunoFontes.Portifolio.ASPView.MvcApplication" Language="C#" %>
